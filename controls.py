87039c12aefdd81084f2a967c5f07c2c13febfca
caihao <caihao.snake@gmail.com>
2020-06-25 13:06:21 -0400

    Create controls.py

---
+++ controls.py
@@ -0,0 +1,36 @@
+# Controls for web app
+
+CATEGORIES = [
+    'All',
+    'Aggregate and synthesize information', 
+    'Understand mechanisms of disease', 
+    'Establish biomarkers', 
+    'Generate data and models', 
+    'Repurpose existing drugs', 
+    'Generate novel drug candidates', 
+    'Validate and optimize drug candidates', 
+    'Design drugs', 
+    'Design preclinical experiments', 
+    'Run preclinical experiments', 
+    'Design clinical trials', 
+    'Recruit for clinical trials', 
+    'Optimize clinical trials', 
+    'Publish data', 
+    'Analyze real world evidence'
+]
+
+REGIONS = {
+    'All': ['South Korea', 'India', 'China', 'Singapore', 'Israel', 'Hong Kong','Australia', 'Japan', \
+            'United States', 'Canada', 'United Kingdom', 'France', 'Germany', 'Norway', 'Finland', 'Ireland', \
+            'The Netherlands', 'Switzerland', 'Austria', 'Hungary', 'Denmark', 'Bulgaria', 'Spain', 'Poland', \
+            'Italy'],
+    'Asia': ['South Korea', 'India', 'China', 'Singapore', 'Israel', 'Hong Kong','Australia', 'Japan'],
+    'North America': ['United States', 'Canada'],
+    'Europe': ['United Kingdom', 'France', 'Germany', 'Norway', 'Finland', 'Ireland', 'The Netherlands', \
+               'Switzerland', 'Austria', 'Hungary', 'Denmark', 'Bulgaria', 'Spain', 'Poland', 'Italy']
+}
+
+METRICS = [
+    "number of startups",
+    "amount ($M.) of investment"
+]
